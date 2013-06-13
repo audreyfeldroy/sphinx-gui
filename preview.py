90e0073a122dcdd8accff85afeea7f7cc892f76a
Audrey Roy <audreyr@gmail.com>
2013-06-14 01:06:09 +0200

    QWebView works with hardcode html local path

--- preview.py
+++ preview.py
@@ -4,4 +4,4 @@ from PySide import QtGui, QtCore, QtWebKit
 class Preview(QtWebKit.QWebView):
 	def __init__(self, parent=None):
 		super(Preview, self).__init__(parent)
-		self.load(QtCore.QUrl('http://www.google.com'))
+		self.load(QtCore.QUrl.fromLocalFile("/Users/audreyr/code/pydream-repos/rstpreviewer/testfiles/contributing.html"))
