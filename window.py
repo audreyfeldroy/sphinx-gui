a02739581d6c9dbde900c226d121b4fb889b4e2d
Audrey Roy <audreyr@gmail.com>
2013-06-14 00:02:12 +0200

    Add splitter with treeview/editor split.

--- window.py
+++ window.py
@@ -1,4 +1,4 @@
-from PySide import QtGui
+from PySide import QtGui, QtCore
 
 from editor import Editor
 
@@ -6,7 +6,13 @@ from editor import Editor
 class MainWindow(QtGui.QMainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
+        splitter = QtGui.QSplitter(QtCore.Qt.Horizontal)
+        treeview =  QtGui.QTreeView()
         editor = Editor()
-        self.setCentralWidget(editor)
+        self.setCentralWidget(splitter)
+
+        splitter.addWidget(treeview)
+        splitter.addWidget(editor)
+
         self.setWindowTitle("RST Previewer")
         self.showMaximized()
