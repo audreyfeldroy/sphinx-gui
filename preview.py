fe07b97223dfffb789611b8b2cd043b628f8cef6
Audrey Roy <audreyr@gmail.com>
2013-06-14 01:11:01 +0200

    Use Unipath for relative paths.

--- preview.py
+++ preview.py
@@ -1,7 +1,11 @@
 from PySide import QtGui, QtCore, QtWebKit
+from unipath import Path
 
 
 class Preview(QtWebKit.QWebView):
 	def __init__(self, parent=None):
 		super(Preview, self).__init__(parent)
-		self.load(QtCore.QUrl.fromLocalFile("/Users/audreyr/code/pydream-repos/rstpreviewer/testfiles/contributing.html"))
+		
+		# TODO: Load HTML from real Sphinx output file
+		output_html_path = Path("testfiles/contributing.html").absolute()
+		self.load(QtCore.QUrl.fromLocalFile(output_html_path))
