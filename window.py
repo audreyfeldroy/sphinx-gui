bdbf951f6f4f87935d1e2d1cfc7b2195a3423486
Audrey Roy <audreyr@gmail.com>
2013-06-14 00:38:46 +0200

    Add File > Open functionality.

--- window.py
+++ window.py
@@ -8,14 +8,47 @@ class MainWindow(QtGui.QMainWindow):
         super(MainWindow, self).__init__(parent)
         splitter = QtGui.QSplitter(QtCore.Qt.Horizontal)
         treeview =  QtGui.QTreeView()
-        editor = Editor()
+        self.editor = Editor()
         self.setCentralWidget(splitter)
         
         # Small treeview, big editor
         treeview.setMaximumWidth(250)
         
         splitter.addWidget(treeview)
-        splitter.addWidget(editor)
+        splitter.addWidget(self.editor)
 
         self.setWindowTitle("RST Previewer")
         self.showMaximized()
+        
+        self.setupActions()
+
+    def setupActions(self):
+        self.fileMenu = self.menuBar().addMenu("&File")
+        self.openAction = QtGui.QAction(
+                                QtGui.QIcon(":/images/open.png"), 
+                                "&Open", 
+                                self, 
+                                shortcut="Ctrl+O",
+                                statusTip="Open File", 
+                                triggered=self.openFile
+                            )
+        self.fileMenu.addAction(self.openAction)
+
+    def openFile(self, path=None):
+        if not path:
+            path = QtGui.QFileDialog.getOpenFileName(self, "Open File",
+                    '', "ReStructuredText Files (*.rst)")
+    
+        if path:
+            inFile = QtCore.QFile(path[0])
+            if inFile.open(QtCore.QFile.ReadOnly | QtCore.QFile.Text):
+                text = inFile.readAll()
+    
+                try:
+                    # Python v3.
+                    text = str(text, encoding='ascii')
+                except TypeError:
+                    # Python v2.
+                    text = str(text)
+    
+                self.editor.setPlainText(text)
